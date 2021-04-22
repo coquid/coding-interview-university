8c8806863d2be16082301db960ad0a31af757cd7
coquid <coquid93@gmail.com>
2021-04-23 00:44:57 +0900

    Create custom list with IList in C#

--- Src/CSharp/CodePractice/CodePractice/DataStructure/Array.cs
+++ Src/CSharp/CodePractice/CodePractice/DataStructure/Array.cs
@@ -1,7 +1,133 @@
 ﻿using System;
+using System.Collections;
+
 namespace CodePractice.DataStructure
 {
-    public struct Array
+    // struct 기반 array
+    public struct SArray : IList
     {
+        object[] items;
+
+		/// <summary>Gets a value indicating whether the <see cref="T:System.Collections.IList" /> has a fixed size.</summary>
+		/// <returns>
+		///   <see langword="true" /> if the <see cref="T:System.Collections.IList" /> has a fixed size; otherwise, <see langword="false" />.</returns>
+		public bool IsFixedSize
+		{
+			get;
+		}
+
+		/// <summary>Gets a value indicating whether the <see cref="T:System.Collections.IList" /> is read-only.</summary>
+		/// <returns>
+		///   <see langword="true" /> if the <see cref="T:System.Collections.IList" /> is read-only; otherwise, <see langword="false" />.</returns>
+		public bool IsReadOnly
+		{
+			get;
+		}
+
+		/// <summary>Gets or sets the element at the specified index.</summary>
+		/// <param name="index">The zero-based index of the element to get or set.</param>
+		/// <exception cref="T:System.ArgumentOutOfRangeException">
+		///   <paramref name="index" /> is not a valid index in the <see cref="T:System.Collections.IList" />.</exception>
+		/// <exception cref="T:System.NotSupportedException">The property is set and the <see cref="T:System.Collections.IList" /> is read-only.</exception>
+		/// <returns>The element at the specified index.</returns>
+		public object? this[int index]
+		{
+			get { return items[index]; }
+			set { }
+		}
+
+		/// <summary>Adds an item to the <see cref="T:System.Collections.IList" />.</summary>
+		/// <param name="value">The object to add to the <see cref="T:System.Collections.IList" />.</param>
+		/// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.IList" /> is read-only.  
+		///
+		///  -or-  
+		///
+		///  The <see cref="T:System.Collections.IList" /> has a fixed size.</exception>
+		/// <returns>The position into which the new element was inserted, or -1 to indicate that the item was not inserted into the collection.</returns>
+		public int Add(object? value) { return 0; }
+
+		/// <summary>Removes all items from the <see cref="T:System.Collections.IList" />.</summary>
+		/// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.IList" /> is read-only.</exception>
+		public void Clear() { }
+
+		/// <summary>Determines whether the <see cref="T:System.Collections.IList" /> contains a specific value.</summary>
+		/// <param name="value">The object to locate in the <see cref="T:System.Collections.IList" />.</param>
+		/// <returns>
+		///   <see langword="true" /> if the <see cref="T:System.Object" /> is found in the <see cref="T:System.Collections.IList" />; otherwise, <see langword="false" />.</returns>
+		public bool Contains(object? value) { return false; }
+
+		/// <summary>Determines the index of a specific item in the <see cref="T:System.Collections.IList" />.</summary>
+		/// <param name="value">The object to locate in the <see cref="T:System.Collections.IList" />.</param>
+		/// <returns>The index of <paramref name="value" /> if found in the list; otherwise, -1.</returns>
+		public int IndexOf(object? value) { return 0; }
+
+		/// <summary>Inserts an item to the <see cref="T:System.Collections.IList" /> at the specified index.</summary>
+		/// <param name="index">The zero-based index at which <paramref name="value" /> should be inserted.</param>
+		/// <param name="value">The object to insert into the <see cref="T:System.Collections.IList" />.</param>
+		/// <exception cref="T:System.ArgumentOutOfRangeException">
+		///   <paramref name="index" /> is not a valid index in the <see cref="T:System.Collections.IList" />.</exception>
+		/// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.IList" /> is read-only.  
+		///
+		///  -or-  
+		///
+		///  The <see cref="T:System.Collections.IList" /> has a fixed size.</exception>
+		/// <exception cref="T:System.NullReferenceException">
+		///   <paramref name="value" /> is null reference in the <see cref="T:System.Collections.IList" />.</exception>
+		public void Insert(int index, object? value) { }
+
+		/// <summary>Removes the first occurrence of a specific object from the <see cref="T:System.Collections.IList" />.</summary>
+		/// <param name="value">The object to remove from the <see cref="T:System.Collections.IList" />.</param>
+		/// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.IList" /> is read-only.  
+		///
+		///  -or-  
+		///
+		///  The <see cref="T:System.Collections.IList" /> has a fixed size.</exception>
+		public void Remove(object? value) { }
+
+		/// <summary>Removes the <see cref="T:System.Collections.IList" /> item at the specified index.</summary>
+		/// <param name="index">The zero-based index of the item to remove.</param>
+		/// <exception cref="T:System.ArgumentOutOfRangeException">
+		///   <paramref name="index" /> is not a valid index in the <see cref="T:System.Collections.IList" />.</exception>
+		/// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.IList" /> is read-only.  
+		///
+		///  -or-  
+		///
+		///  The <see cref="T:System.Collections.IList" /> has a fixed size.</exception>
+		public void RemoveAt(int index) { }
+
+		public void CopyTo(Array array, int index)
+		{
+		}
+
+		public int Count
+		{
+			get;
+		}
+
+		public bool IsSynchronized
+		{
+			get;
+		}
+
+		// Return the current instance since the underlying store is not
+		// publicly available.
+		public object SyncRoot
+		{
+			get;
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			// Refer to the IEnumerator documentation for an example of
+			// implementing an enumerator.
+			throw new NotImplementedException("The method or operation is not implemented.");
+		}
+
+	}
+
+	// class 기반 array
+	public class CArray
+    {
+
     }
 }
